3b94bb83714b591ea396a9eff3ff92279819c2e8
ChrisMMorrison <chris.morrison0212@gmail.com>
2018-03-02 13:41:36 -0500

    Modify Test File

--- src/GitTest/Test.java
+++ src/GitTest/Test.java
@@ -3,8 +3,8 @@ package GitTest;
 public class Test {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
+        for (int i = 48; i < 100; i++) {
+            System.out.println((char)(i));
         }
     }
 }
